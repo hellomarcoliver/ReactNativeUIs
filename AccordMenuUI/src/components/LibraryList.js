a019c01f3b0ef98089273cc5dff06f47db1a6d1c
Marc-Oliver Gern <mo.gern@gmail.com>
2017-09-13 10:08:35 +0100

    Make list of titles show up in app

--- AccordMenuUI/src/components/LibraryList.js
+++ AccordMenuUI/src/components/LibraryList.js
@@ -1,20 +1,39 @@
 /* Purpose: Render the list of libraries */
 
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 //here, we need to reach out to the data –the state (.json)
 class LibraryList extends Component {
+  componentWillMount() {
+    //initialize data list view (this can be copied)
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(this.props.dataToShow1);
+  }
+
+  //render a specific row
+  renderRow(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    // console.log(this.props);
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
 //take our global state object, map it and provide them as props to our
 //component of LibraryList
 const mapStateToProps = state => {
-  // console.log(state);
   //this is an object with key of dataToShow1 and current state
   //this gets passed on to the component above
   return { dataToShow1: state.dataToShow1 };
---
+++ AccordMenuUI/src/components/ListItem.js
@@ -0,0 +1,19 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+
+class ListItem extends Component {
+  render() {
+    return (
+      <CardSection>
+        <Text>{this.props.library.title}</Text>
+      </CardSection>
+    );
+  }
+
+}
+
+export default ListItem;

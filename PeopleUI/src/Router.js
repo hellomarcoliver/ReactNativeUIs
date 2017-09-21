030ef6d134a668b30fcedb0fdcb9b57da892f817
Marc-Oliver Gern <mo.gern@gmail.com>
2017-09-21 08:55:42 +0100

    Change from React Navigation to Router Flux

--- PeopleUI/src/Router.js
+++ PeopleUI/src/Router.js
@@ -1,20 +1,43 @@
 /* manage all scenes/views from the app */
 
 import React from 'react';
-import { Stack, Scene, Router } from 'react-native-router-flux';
+import { Stack, Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+// import EmployeeCreate from './components/EmployeeCreate';
+// import EmployeeEdit from './components/EmployeeEdit';
+
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{ paddingTop: 30 }}>
-      <Stack key="root">
-        <Scene key="login" component={LoginForm} title="Login" />
-        <Scene key='employeeList' component={EmployeeList} title="Employees" />
-      </Stack>
+    <Router>
+      <Stack key="root" modal hideNavBar>
+        <Scene key="auth" initial title="Login" >
+          <Scene key="login" component={LoginForm} />
+        </Scene>
+        <Scene key="main">
+          <Scene
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            initial
+          />
+          {/* <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+          <Scene
+            key='employeeEdit'
+            component={EmployeeEdit}
+            title='Employee Edit'
+          /> */}
+        </Scene>
+    </Stack>
     </Router>
   );
 };
-
 
 export default RouterComponent;

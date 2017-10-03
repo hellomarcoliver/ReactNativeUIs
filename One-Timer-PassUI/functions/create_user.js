e06c9a35c089bed3cca8b2edddb5dde4925ec8c2
Marc-Oliver Gern <mo.gern@gmail.com>
2017-10-03 12:24:09 +0100

    Add ability to create user via phone number

--- One-Timer-PassUI/functions/create_user.js
+++ One-Timer-PassUI/functions/create_user.js
@@ -1,3 +1,19 @@
-module.exports = function (req, res) {
-  req.send(req.body);
+// needed to ammend firebase
+const admin = require('firebase-admin');
+
+module.exports = function(req, res) {
+  // verify the user provided a phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input. Try Again.' });
+  }
+
+  // format the number and remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // create a new user account using the number
+  admin.auth().createUser({ uid: phone })
+  .then(user => res.send(user))
+  .catch(err => res.status(422).send({ error: err }));
+
+  // respond to the user request, saying the account was made
 }

e9e84dddcd481eab3ea674ae75edb2d7ec00a53c
Marc-Oliver Gern <mo.gern@gmail.com>
2017-10-18 17:47:41 +0100

    Add currency equation

--- CurrencyConverterUI/app/screens/Home.js
+++ CurrencyConverterUI/app/screens/Home.js
@@ -5,11 +5,15 @@ import { StatusBar, KeyboardAvoidingView } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_LAST_CONVERTED = new Date();
+const TEMP_CONVERSION_RATE = 0.79739;
 
 class Home extends Component {
   handleChangeText = () => {
@@ -22,6 +26,10 @@ class Home extends Component {
 
   handlePressQuoteCurrency = () => {
     console.log('press quote currency');
+  };
+
+  handleSwapCurrency = () => {
+    console.log('press swap currency');
   };
 
   render() {
@@ -42,6 +50,13 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           value={TEMP_QUOTE_PRICE}
         />
+        <LastConverted
+          date={TEMP_LAST_CONVERTED}
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          conversionRate={TEMP_CONVERSION_RATE}
+        />
+        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
       </Container>
     );
   }

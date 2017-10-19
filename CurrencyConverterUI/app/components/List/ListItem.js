d6e70e4f7c714180fb1899d1b505810f201e9f05
Marc-Oliver Gern <mo.gern@gmail.com>
2017-10-19 10:38:29 +0100

    Added color settings screen

--- CurrencyConverterUI/app/components/List/ListItem.js
+++ CurrencyConverterUI/app/components/List/ListItem.js
@@ -6,12 +6,18 @@ import styles from './styles';
 import Icon from './Icon';
 
 const ListItem = ({
-  text, onPress, checkmark = true, selected = false, visible = true,
+  text,
+  onPress,
+  checkmark = true,
+  selected = false,
+  visible = true,
+  customIcon = null, // this tells Options.js to render/not-render the icons
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
       {selected ? <Icon visible={visible} checkmark={checkmark} /> : <Icon />}
+      {customIcon}
     </View>
   </TouchableHighlight>
 );
@@ -22,6 +28,7 @@ ListItem.propTypes = {
   checkmark: PropTypes.bool,
   selected: PropTypes.bool,
   visible: PropTypes.bool,
+  customIcon: PropTypes.element,
 };
 
 export default ListItem;
--- CurrencyConverterUI/app/index.js
+++ CurrencyConverterUI/app/index.js
@@ -2,21 +2,26 @@
 
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import CurrencyList from './screens/CurrencyList';
+import Themes from './screens/Themes';
 
 // creating variables for styles
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+
   $white: '#FFFFFF',
+  $lightGray: '#F0F0F0',
   $border: '#E2E2E2',
   $inputText: '#797979',
-  $lightGrey: '#F0F0F0',
   $darkText: '#343434',
+
   // this adds an outline to all components for better debugging
   outline: 0,
 });
 
-export default () => <CurrencyList />;
+export default () => <Themes />;
 
 // new version
 // const App = () => (
